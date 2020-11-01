a14452e25f1b48858c14633f4a235b554af4b9dd
MaulingMonkey <git@maulingmonkey.com>
2020-11-01 15:58:13 -0800

    Test multiple distros

--- src/_lib.rs
+++ src/_lib.rs
@@ -5,28 +5,33 @@
 //!
 //! ```rust
 //! use wslapi::*;
-//! 
-//! let ubuntu = "Ubuntu";
-//! let nonexistant = "Nonexistant";
-//! 
+//!
 //! let wsl = Library::new().unwrap();
-//! assert!( wsl.is_distribution_registered(ubuntu));
+//!
+//! let nonexistant = "Nonexistant";
 //! assert!(!wsl.is_distribution_registered(nonexistant));
 //! assert!(wsl.get_distribution_configuration(nonexistant).is_err());
-//! 
-//! let cfg = wsl.get_distribution_configuration(ubuntu).unwrap();
-//! assert!(cfg.default_uid == 0 || (1000 ..= 2000).contains(&cfg.default_uid));
-//! // 0 == root, 1000+ == regular user
-//! assert!(cfg.flags & WSL_DISTRIBUTION_FLAGS::DEFAULT == WSL_DISTRIBUTION_FLAGS::DEFAULT);
-//! // `cfg.flags` contains extra, undocumented flags like 0x8
-//! assert!((1..2).contains(&cfg.version)); // WSL version
 //!
-//! wsl.launch_interactive(ubuntu, "echo testing 123", true).unwrap();
-//! 
-//! let stdin  = "echo testing 456\necho PATH: ${PATH}\n";
-//! let stdout = std::fs::File::create("target/basic.txt").unwrap();
-//! let stderr = (); // shorthand for Stdio::null()
-//! wsl.launch(ubuntu, "sh", true, stdin, stdout, stderr).unwrap().wait().unwrap();
+//! let mut found = 0;
+//! for ubuntu in ["Ubuntu", "Ubuntu-18.04", "Ubuntu-16.04"].iter().copied() {
+//!     if !wsl.is_distribution_registered(ubuntu) { continue }
+//!     found += 1;
+//!
+//!     let cfg = wsl.get_distribution_configuration(ubuntu).unwrap();
+//!     assert!(cfg.default_uid == 0 || (1000 ..= 2000).contains(&cfg.default_uid));
+//!     // 0 == root, 1000+ == regular user
+//!     assert!(cfg.flags & WSL_DISTRIBUTION_FLAGS::DEFAULT == WSL_DISTRIBUTION_FLAGS::DEFAULT);
+//!     // `cfg.flags` contains extra, undocumented flags like 0x8
+//!     assert!((1..2).contains(&cfg.version)); // WSL version
+//!
+//!     wsl.launch_interactive(ubuntu, "echo testing 123", true).unwrap();
+//!
+//!     let stdin  = "echo testing 456\necho PATH: ${PATH}\n";
+//!     let stdout = std::fs::File::create("target/basic.txt").unwrap();
+//!     let stderr = (); // shorthand for Stdio::null()
+//!     wsl.launch(ubuntu, "sh", true, stdin, stdout, stderr).unwrap().wait().unwrap();
+//! }
+//! assert_ne!(0, found, "Found {} ubuntu distros", found);
 //! ```
 //!
 //! [wslapi.h]:     https://docs.microsoft.com/en-us/windows/win32/api/wslapi/
