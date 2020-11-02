f0db9372c0634497ee2c1e3405a9edfb5a4ce488
MaulingMonkey <git@maulingmonkey.com>
2020-11-01 21:13:38 -0800

    EnvironmentVariables: finish

--- examples/basic.rs
+++ examples/basic.rs
@@ -16,6 +16,14 @@ fn main() {
     assert!(cfg.flags & WSL_DISTRIBUTION_FLAGS::DEFAULT == WSL_DISTRIBUTION_FLAGS::DEFAULT);
     assert!((1..=2).contains(&cfg.version)); // WSL version
 
+    println!("Environment varibales:");
+    for (k,v) in cfg.default_environment_variables.iter() {
+        // XXX: This might be inaccurate / depend on the linux locale?  Good enough for a demo though!
+        let k = String::from_utf8_lossy(k);
+        let v = String::from_utf8_lossy(v);
+        println!("{: <10} = {}", k, v);
+    }
+
     wsl.launch_interactive(ubuntu, "echo testing 123", true).unwrap();
 
     let stdin  = "echo testing 456\necho PATH: ${PATH}\nasdf";
@@ -23,5 +31,6 @@ fn main() {
     let stderr = std::fs::File::create("CON").unwrap();
     wsl.launch(ubuntu, "sh", true, stdin, stdout, stderr).unwrap().wait().unwrap();
 
-    std::thread::sleep(std::time::Duration::from_millis(1000));
+    println!("\rPress ENTER to quit");
+    let _ = std::io::stdin().read_line(&mut String::new());
 }
--- src/configuration.rs
+++ src/configuration.rs
@@ -1,10 +1,15 @@
 use crate::WSL_DISTRIBUTION_FLAGS;
 
-use winapi::shared::ntdef::ULONG;
-use winapi::shared::ntdef::PSTR;
+use winapi::shared::ntdef::{PSTR, ULONG};
 use winapi::um::combaseapi::CoTaskMemFree;
 
+use std::ops::Drop;
 use std::ptr::null_mut;
+
+// winapi uses `CHAR` = `c_char` = `i8`, but `u8` is way more rusty considering
+// the env vars in question might actually be UTF8, so I expose `&[u8]` instead
+// of `&[CHAR]` / `&[i8]`.  Of course, they could be ASCII or another locale
+// entirely as well, so I don't try to `&str`ify the env strings in question.
 
 
 
@@ -28,9 +33,7 @@ pub struct Configuration {
 
 
 
-/// <span style="opacity: 33%">(TODO)</span>
 /// The environment variables of [WslGetDistributionConfiguration].
-/// Not yet implemented.
 /// 
 /// [WslGetDistributionConfiguration]:      https://docs.microsoft.com/en-us/windows/win32/api/wslapi/nf-wslapi-wslgetdistributionconfiguration
 pub struct EnvironmentVariables {
@@ -44,13 +47,43 @@ impl EnvironmentVariables {
 
     /// Get the number of environment variables
     pub fn len(&self) -> usize { self.count }
+
+    /// Get the key/value pair at `index`
+    pub fn get(&self, index: usize) -> Option<(&[u8], &[u8])> {
+        if index >= self.count {
+            None
+        } else {
+            let st = unsafe { *self.array.add(index) };
+
+            for i in 0.. {
+                match unsafe { *st.add(i) } as _ {
+                    b'\0' => return Some((unsafe { std::slice::from_raw_parts(st.cast(), i) }, &[])),
+                    b'=' => for k in i.. {
+                        if unsafe { *st.add(k) } != 0 { continue }
+
+                        let all = unsafe { std::slice::from_raw_parts(st.cast(), k) };
+                        let (k, v) = all.split_at(i);
+                        return Some((k, &v[1..]));
+                    },
+                    _ => {},
+                }
+            }
+
+            None // unreachable
+        }
+    }
+
+    /// Iterate over the key/value pairs
+    pub fn iter(&self) -> impl Iterator<Item = (&[u8], &[u8])> {
+        EnvironmentVariablesIter { vars: self, index: 0 }
+    }
 }
 
 impl Default for EnvironmentVariables {
     fn default() -> Self { Self::new() }
 }
 
-impl std::ops::Drop for EnvironmentVariables {
+impl Drop for EnvironmentVariables {
     fn drop(&mut self) {
         // "The caller is responsible for freeing each string in pDefaultEnvironmentVariablesArray (and the array itself) via CoTaskMemFree."
         // https://docs.microsoft.com/en-us/windows/win32/api/wslapi/nf-wslapi-wslgetdistributionconfiguration
@@ -64,5 +97,30 @@ impl std::ops::Drop for EnvironmentVariables {
 
         self.array = null_mut();
         self.count = 0;
+    }
+}
+
+impl<'e> IntoIterator for &'e EnvironmentVariables {
+    type Item = (&'e [u8], &'e [u8]);
+    type IntoIter = EnvironmentVariablesIter<'e>;
+    fn into_iter(self) -> Self::IntoIter {
+        EnvironmentVariablesIter { vars: self, index: 0 }
+    }
+}
+
+
+
+/// Iterator over &[EnvironmentVariables]
+pub struct EnvironmentVariablesIter<'e> {
+    vars:   &'e EnvironmentVariables,
+    index:  usize,
+}
+
+impl<'e> Iterator for EnvironmentVariablesIter<'e> {
+    type Item = (&'e [u8], &'e [u8]);
+    fn next(&mut self) -> Option<Self::Item> {
+        let kv = self.vars.get(self.index)?;
+        self.index += 1;
+        Some(kv)
     }
 }
